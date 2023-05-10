fd1bed98a6f59c0c1eff21abfffb5f069cf26d55
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2023-05-10 14:39:59 +0300

    Problem #62

--- Spiral Matrix/Algo.cpp
+++ Spiral Matrix/Algo.cpp
@@ -15,30 +15,33 @@ int main() {
 vector<int> spiralOrder(vector<vector<int>>& matrix) {
     int n = matrix.size(), m = matrix[0].size();
     if (n == 0 || m == 0) {
-            return vector<int>();
+        return vector<int>();
     }
-    vector<int> output(n*m);
-    int idx = 0;
+    vector<int> output;
     int bottom  = n - 1, right = m - 1;
     int left = 0, top = 0;
 
-    while (idx < n*m)
+    while (output.size() < n*m)
     {
         for(int i = left; i <= right; i++)
-            output[idx++] = matrix[top][i];
+            output.push_back(matrix[top][i]);
         top++;
 
         for(int i = top; i <= bottom; i++)
-            output[idx++] = matrix[i][right];
+            output.push_back(matrix[i][right]);
         right--;
 
-        for(int i = right; i >= left; i--)
-            output[idx++] = matrix[bottom][i];
-        bottom--;
+        if(top <= bottom) {
+            for(int i = right; i >= left; i--)
+                output.push_back(matrix[bottom][i]);
+            bottom--;
+        }
 
-        for(int i = bottom; i >= top; i--)
-            output[idx++] = matrix[i][left];
-        left++;  
+        if(left <= right) {
+            for(int i = bottom; i >= top; i--)
+                output.push_back(matrix[i][left]);
+            left++;  
+        }
     }
     
     return output;
